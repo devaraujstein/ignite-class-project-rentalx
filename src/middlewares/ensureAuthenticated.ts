b5c13eb520df582b4d938327069984f474c779ca
Andre Araujo <andreearaujo98@gmail.com>
2021-03-26 22:52:52 -0300

    user, bcrypt, jwt, exceptions

---
+++ src/middlewares/ensureAuthenticated.ts
@@ -0,0 +1,42 @@
+import { Request, Response, NextFunction } from "express";
+import { verify } from "jsonwebtoken";
+
+import { AppError } from "../errors/AppError";
+import { UsersRepository } from "../modules/accounts/repositories/implementations/UsersRepository";
+
+interface IPayLoad {
+  sub: string;
+}
+
+export async function ensureAuthenticated(
+  request: Request,
+  response: Response,
+  next: NextFunction
+) {
+  const authHeader = request.headers.authorization;
+
+  if (!authHeader) {
+    throw new AppError("Token missing", 401);
+  }
+
+  const [, token] = authHeader.split(" ");
+
+  try {
+    const { sub: user_id } = verify(
+      token,
+      "7fc076cf94b177819f802a989ddcb535"
+    ) as IPayLoad;
+
+    const usersRepository = new UsersRepository();
+
+    const user = await usersRepository.findById(user_id);
+
+    if (!user) {
+      throw new AppError("User does not exists!", 401);
+    }
+
+    next();
+  } catch {
+    throw new AppError("Invalid token !", 401);
+  }
+}
