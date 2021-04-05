97512f501032bd6b48186f064b6dcbe386ff2e14
Andre Araujo <andreearaujo98@gmail.com>
2021-04-05 15:47:22 -0300

    rentals

--- src/shared/infra/typeorm/migrations/1616774104461-CreateUsers.ts
+++ src/shared/infra/typeorm/migrations/1616774104461-CreateUsers.ts
@@ -9,6 +9,7 @@ export class CreateUsers1616774104461 implements MigrationInterface {
           {
             name: "id",
             type: "uuid",
+            isPrimary: true,
           },
           {
             name: "name",
