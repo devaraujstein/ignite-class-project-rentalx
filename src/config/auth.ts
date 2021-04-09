29aaf96a1aeca6c20603fa60ac6604c28935242b
Andre Araujo <andreearaujo98@gmail.com>
2021-04-09 13:04:12 -0300

    autentication

---
+++ src/config/auth.ts
@@ -0,0 +1,7 @@
+export default {
+  secret_token: "7fc076cf94b177819f802a989ddcb535",
+  expires_in_token: "15m",
+  secret_refresh_token: "65e8dcce94b5c387576b7047b198ce9d",
+  expires_in_refresh_token: "30d",
+  expires_refresh_token_days: 30,
+};
